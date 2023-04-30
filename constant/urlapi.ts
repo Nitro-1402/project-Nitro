826dc7bc0364280ad67529299663b15e609af5e3
ali attarian <ali.live26.aa@gmail.com>
2023-04-30 13:07:58 +0330

    fix: cors error fixed

--- constant/urlapi.ts
+++ constant/urlapi.ts
@@ -1,2 +1,2 @@
-export  const URL_API='http://localhost:8000/'
-export const baseURL='https://nitroback.pythonanywhere.com/'
+// export  const URL_API='http://localhost:8000/'
+export const URL_API='https://nitroback.pythonanywhere.com/'
