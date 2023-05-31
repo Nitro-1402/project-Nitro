b1a23cba76b3162665a9b03ab593bf8dfb7bec3a
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2023-05-31 14:04:31 +0330

    Feat/Criticism page and post content

---
+++ component/Critisism/PostCard.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styles from "@/styles/PostCard.module.css";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import {Link} from '@mui/material'
+
+interface User {
+  first_name: string;
+  last_name: string;
+  username: string;
+  photo: string;
+}
+interface ItemProps {
+  user?: User;
+  postImg?: string;
+  isPremium?: boolean;
+  postContent?: string;
+  postLikes?: number;
+  
+}
+
+const PostCard: React.FC<ItemProps> = ({
+  user = {
+    first_name: "عارف",
+    last_name: "علیجان نژاد",
+    username: "aref_alj",
+    photo: "/image/user.png",
+  },
+  postImg = "/image/test.jpeg",
+  isPremium = false,
+  postContent = "",
+  postLikes = 0,
+}) => {
+  return (
+    <div className={styles.postContainer}>
+      <div className={styles.postHeader}>
+        <img
+          className={styles.posterUserImg}
+          src={user.photo}
+          alt={user.username}
+        />
+        <div className={styles.postedUserName}>
+          <span>
+            {user.first_name} {user.last_name}
+          </span>
+        </div>
+        <div className={styles.postedUserUsername}>
+          <Link href='/myprofile'>@{user.username}</Link>
+        </div>
+      </div>
+      <div className={styles.postContents}>
+        {/* <h5>{postContent}</h5> */}
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui quis
+          quos earum libero veritatis maiores officiis sapiente nam, ut omnis
+          repellat eveniet labore ullam. Accusantium recusandae atque libero
+          quos aspernatur.
+        </p>
+      </div>
+      <div className={styles.postImage}>
+        {/* <p>{postImg}</p> */}
+        <img src={postImg} alt="" />
+      </div>
+      <div className={styles.postDetails}>
+        <button style={{border:"2px solid #faa500",backgroundColor:"#24282A",color:"#fff"}}>
+          <MdOutlineFavoriteBorder style={{color:"#faa500",marginLeft:"20px",marginTop:"2px"}} />
+          {postLikes}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
---
+++ pages/critisisms.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+import PostCard from "@/component/Critisism/PostCard";
+import { styled } from "@mui/material/styles";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Footer from "@/component/Footer";
+import Header from "@/component/header";
+import styles from "../styles/Critisism.module.css";
+
+const StyledTabs = styled((props) => (
+  <Tabs
+    sx={{ display: "flex", justifyContent: "center" }}
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+  />
+))({
+  "& .MuiTabs-flexContainer": {
+    justifyContent: "center",
+    marginTop: "20px",
+    // marginBottom: '20px',
+  },
+  "& .MuiTabs-indicator": {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    // alignItems: 'center',
+  },
+  "& .MuiTabs-indicatorSpan": {
+    maxWidth: 40,
+    width: "100%",
+    // backgroundColor: '#635ee7',
+    backgroundColor: "#faa500",
+  },
+});
+
+const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: "none",
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(18),
+    marginRight: theme.spacing(1),
+    color: "#fff",
+    fontFamily: "RegularPersian",
+    "&.Mui-selected": {
+      color: "#faa500",
+    },
+    "&.Mui-focusVisible": {
+      backgroundColor: "#fff",
+    },
+  })
+);
+
+const TabPanel = (props:any) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+      style={{display:"flex",justifyContent:"center"}}
+    >
+      {value === index && (
+        <Box sx={{
+
+        }}>
+          <Typography
+            sx={{
+              paddingTop: "20px",
+              paddingLeft: "30px",
+              paddingRight: "30px",
+              
+            }}
+          >
+            {children}
+          </Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+const Critisism = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event:any,newValue:any) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={styles.critisismWrapper}>
+      <Header />
+      <Box
+        sx={{
+          bgcolor: "#0c1012",
+          borderRadius: "8px",
+          paddingBottom: "30px",
+          marginTop: "75px",
+        }}
+      >
+        <StyledTabs
+          value={value}
+          onChange={handleChange}
+          aria-label="styled tabs example"
+        >
+          <StyledTab label="همه پست‌ها" />
+          <StyledTab label="برای شما" />
+        </StyledTabs>
+        <TabPanel value={value} index={0}>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+        </TabPanel>
+        <TabPanel value={value} index={1}></TabPanel>
+      </Box>
+      <Footer />
+    </div>
+  );
+};
+
+export default Critisism;
